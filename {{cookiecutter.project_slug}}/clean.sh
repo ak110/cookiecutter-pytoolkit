91152754823a9b022b619b6f912f377c742cc1de
aki <mark@aur.ll.to>
2020-04-09 06:32:50 +0900

    .

--- {{cookiecutter.project_slug}}/clean.sh
+++ {{cookiecutter.project_slug}}/clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# 対応するpyファイルの無いmodels配下のディレクトリがあれば ___trash___ 配下へ移動するスクリプト。
+# 対応するpyファイルの無いmodels配下のディレクトリがあれば _trash 配下へ移動するスクリプト。
 # 逆の復元もする。
 #
 # 前提とする命名規則:
@@ -8,18 +8,18 @@
 #
 set -eu
 
-mkdir -p ___trash___/models/
+mkdir -p _trash/models/
 
 for d in models/* ; do
     if [ ! -f "$(basename $d).py" ] ; then
-        mv $d ___trash___/models/
+        mv $d _trash/models/
         echo "'$d' removed."
     fi
 done
 
 for f in *.py ; do
-    if [ ! -d "models/$(basename $f .py)" -a -d "___trash___/models/$(basename $f .py)" ] ; then
-        mv "___trash___/models/$(basename $f .py)" models/
+    if [ ! -d "models/$(basename $f .py)" -a -d "_trash/models/$(basename $f .py)" ] ; then
+        mv "_trash/models/$(basename $f .py)" models/
         echo "'models/$(basename $f .py)' restored."
     fi
 done
