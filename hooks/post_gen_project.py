9d7b5f1918ebe1f2633d5f13a39354b7cbe4caa0
aki <mark@aur.ll.to>
2021-10-09 17:43:02 +0900

    .

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
+"""プロジェクト作成後の処理。"""
+import os
 import subprocess
 
 
 def _main():
+    url = "git@github.com:ak110/pytoolkit.git"
+    url = os.environ.get("_PYTOOLKIT_URL", url)
+
     # pytoolkitを追加してInitial commit
     subprocess.run("git init", shell=True, check=True)
-    subprocess.run(
-        "git submodule add git@github.com:ak110/pytoolkit.git pytoolkit.git",
-        shell=True,
-        check=True,
-    )
+    subprocess.run(f"git submodule add {url} pytoolkit.git", shell=True, check=True)
     subprocess.run("ln -s pytoolkit.git/pytoolkit", shell=True, check=True)
     subprocess.run("git add -A", shell=True, check=True)
     subprocess.run('git commit -m "Initial commit."', shell=True, check=True)
